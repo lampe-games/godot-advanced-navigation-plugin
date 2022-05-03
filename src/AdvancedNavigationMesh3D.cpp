869c738a70b7c2a217729207baa1e3b500706817
Pawel Lampe <pawel.lampe@gmail.com>
2022-05-03 22:07:17 +0200

    Make 'AdvancedNavigationMesh3D' a tool

--- src/AdvancedNavigationMesh3D.cpp
+++ src/AdvancedNavigationMesh3D.cpp
@@ -1,11 +1,17 @@
 #include "AdvancedNavigationMesh3D.hpp"
 
+void AdvancedNavigationMesh3D::_init()
+{
+  Godot::print("AdvancedNavigationMesh3D::_init: .cpp");
+}
+
 void AdvancedNavigationMesh3D::_ready()
 {
   Godot::print("AdvancedNavigationMesh3D::_ready: .cpp");
 }
 
-void AdvancedNavigationMesh3D::_init()
+void AdvancedNavigationMesh3D::_process()
 {
-  Godot::print("AdvancedNavigationMesh3D::_init: .cpp");
+  Godot::print("AdvancedNavigationMesh3D::_process: .cpp");
+  set_process(false);
 }
--- src/AdvancedNavigationMesh3D.hpp
+++ src/AdvancedNavigationMesh3D.hpp
@@ -13,10 +13,11 @@ class AdvancedNavigationMesh3D : public Spatial
 
   void _init(); // `_init` must exist as it is called by Godot.
   void _ready();
+  void _process();
 
   static void _register_methods()
   {
     register_method("_ready", &AdvancedNavigationMesh3D::_ready);
-    ;
+    register_method("_process", &AdvancedNavigationMesh3D::_process);
   }
 };
